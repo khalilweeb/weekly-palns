c66b8d98b556ebaac7b7d4240be84ad62b910bdc
khalilweeb <khalilazaez@gmail.com>
2023-12-22 15:40:35 +0100

    adding the header component

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import clss from './Header.module.css';
+
+const Header = () => {
+
+    return (
+        <header className={clss.header}>
+            <h1>Weekly Planer</h1>
+        </header>
+    )
+
+}
+
+export default Header;
