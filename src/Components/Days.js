e5afe014a76d2c957aab64dc765f39e55268db27
khalilweeb <khalilazaez@gmail.com>
2023-12-25 20:54:34 +0100

    adding the icon logic

--- src/Components/Days.js
+++ src/Components/Days.js
@@ -1,15 +1,23 @@
+import { useState } from 'react';
 import clss from './Days.module.css';
-import { FaAngleDown , FaAngleUp  } from "react-icons/fa6";
+import {  FaAngleUp  } from "react-icons/fa6";
 
 
 
 
 const Days = props => {
+    const [up , setUp] = useState(false);
+
+    const iconClickHandler = () => {
+       up ? setUp(false) : setUp(true);
+    }   
+
 
 return (
     <div>
         <div className={clss.daydv}>
-            <h2 className={clss.day}>{props.day}</h2>    <FaAngleDown className={clss.icon} />
+            <h2 className={clss.day}>{props.day}</h2>    
+            <FaAngleUp className={`${clss.icon} ${ up && clss.rotate }`} onClick={iconClickHandler}/>
         </div>
     </div>
 )
